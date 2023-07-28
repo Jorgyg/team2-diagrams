d023f4b7a01c7fb760ac1330eaf905c0c458071c
Sergio Gallego Gudiño <118269684+SergioGallegoGudino@users.noreply.github.com>
2023-07-28 14:53:13 +0200

    Update Ej12.sql

--- scripts-sql/Ej12.sql
+++ scripts-sql/Ej12.sql
@@ -35,3 +35,19 @@ create table trabajadores (
 	FOREIGN KEY (id_proyecto) references proyectos(id) on delete cascade on update cascade,
 	FOREIGN KEY (codigo_profesion) references profesion(codigo_profesion) on delete cascade on update cascade
 );
+insert into empresas (cif,nombre,direccion,telefono,codigo_empresa) values 
+(0123456789,'tech-system','direccion1',666999888,1),
+(0112344566,'orgSystems','direccion2',663258258,2),
+(1122856123,'otraMasSystem','direccion3',654124784,3);
+insert into profesion (codigo_profesion,nombre) values 
+(1,'diseñador visual'),
+(2,'analista'),
+(3,'programador');
+insert into proyectos (id,fecha_inicio,fecha_final,fecha_final_prevista,cif_empresa) values 
+(1,'2022-02-27','2022-04-20','2022-03-31',0123456789),
+(2,'2022-12-12','2023-01-24','2023-01-31',1122856123);
+insert into trabajadores (codigo_trabajador,dni,nombre,apellidos,profesion,id_proyecto,horas_trabajadas,codigo_profesion) values 
+(1,'147258369i','Pedro','Garcia Garcia','diseñador visual',1,113,1),
+(2,'14582369r','Laura','Fernandez Perez','analista',1,33,1),
+(3,'123458980t','Javier','Gonzalez Ruiz','programador',1,80,1);
+update trabajadores set horas_trabajadas=120 where codigo_trabajador=1;
