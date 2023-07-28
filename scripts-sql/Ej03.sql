8e74b99dc22eb963104560e9c45b0bab6fcb3d2e
Sergio Gallego Gudiño <118269684+SergioGallegoGudino@users.noreply.github.com>
2023-07-28 14:53:46 +0200

    Update Ej03.sql

--- scripts-sql/Ej03.sql
+++ scripts-sql/Ej03.sql
@@ -37,6 +37,33 @@ create table es_capital (
 	FOREIGN KEY (codigo_provincia) references provincias(codigo_postal) on delete cascade on update cascade
 );
 
-
+insert into comunidades (poblacion,superficie) values 
+(130000,60),
+(200000,50),
+(300000,73),
+(6000,24),
+(100500,66);
+insert into provincias (codigo_postal,nombre,poblacion,superficie,id_comunidad) values 
+(4000,'tarragona',132000,800,1),
+(2000,'almeria',66000,850,3),
+(0800,'madrid',100000,1000,2),
+(1500,'a coruña',77000,900,4),
+(1000,'valladolid',71000,800,5);
+insert into localidades (nombre,poblacion,codigo_provincia) values 
+('tarragona',132000,1),
+('almeria',66000,2),
+('madrid',100000,3),
+('a coruña',77000,4),
+('valladolid',71000,5);
+insert into es_capital (id_localidad,codigo_provincia) values 
+(1,4000),
+(2,800),
+(3,2000),
+(4,1500);
+insert into es_capital_CA (id_ca,id_localidad,es_cap) values 
+(1,1,true),
+(2,2,false),
+(3,3,false),
+(4,4,true);
 
 
