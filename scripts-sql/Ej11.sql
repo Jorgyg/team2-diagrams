286cab49fab5e371367835efbdb972d8c6f8ae05
Sergio Gallego Gudiño <118269684+SergioGallegoGudino@users.noreply.github.com>
2023-07-28 14:55:37 +0200

    Update Ej11.sql

--- scripts-sql/Ej11.sql
+++ scripts-sql/Ej11.sql
@@ -71,3 +71,23 @@ CREATE TABLE Montañas (
     FOREIGN KEY (id_acc_geo) REFERENCES Accidentes_geograficos(id)
      ON DELETE CASCADE ON UPDATE CASCADE
 );
+INSERT INTO Paises (nombre, extension, poblacion) VALUES ('País X', 10000, 50000000);
+INSERT INTO Paises (nombre, extension, poblacion) VALUES ('País Y', 8000, 40000000);
+
+INSERT INTO Ciudades (nombre, extension, poblacion, id_pais) VALUES ('Ciudad A', 500, 1000000, 2);
+INSERT INTO Ciudades (nombre, extension, poblacion, id_pais) VALUES ('Ciudad B', 300, 800000, 1);
+
+INSERT INTO Accidentes_geograficos (nombre, posicion_h, posicion_v) VALUES ('Accidente 1', 40.5, 70.2);
+INSERT INTO Accidentes_geograficos (nombre, posicion_h, posicion_v) VALUES ('Accidente 2', 35.2, -80.1);
+INSERT INTO Accidentes_geograficos (nombre, posicion_h, posicion_v) VALUES ('Accidente 3', 38.7, 65.8);
+INSERT INTO Accidentes_geograficos (nombre, posicion_h, posicion_v) VALUES ('Accidente 4', 900.7, 200);
+
+INSERT INTO Rios(id_acc_geo, longitud) VALUES (1, 300);
+INSERT INTO Montañas(id_acc_geo, altura) VALUES (2, 250);
+INSERT INTO Lagos(id_acc_geo, extension) VALUES (3, 20);
+INSERT INTO Rios(id_acc_geo, longitud) VALUES (4, 1000);
+
+INSERT INTO PasaPor(id_ciudad, id_rio) VALUES (1,2);
+INSERT INTO PasaPor(id_ciudad, id_rio) VALUES (2,1);
+
+DELETE FROM Accidentes_geograficos WHERE id = 1;
