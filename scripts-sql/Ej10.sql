d6796ca69bae226f862cbd65d50d2be4e30b53c9
Sergio Gallego Gudiño <118269684+SergioGallegoGudino@users.noreply.github.com>
2023-07-28 14:52:43 +0200

    Update Ej10.sql

--- scripts-sql/Ej10.sql
+++ scripts-sql/Ej10.sql
@@ -46,3 +46,23 @@ create table entrenadores (
     primary key(dni),
 	FOREIGN KEY (id_equipo) references equipos(id) on delete cascade on update cascade
 );
+
+insert into equipos (id,nombre,estadio) values 
+(1,'real madrid','santiago bernabeu'),
+(2,'fc barcelona','camp nou'),
+(3,'at madrid','metropolitano'),
+(4,'sevilla','sanchez pijuan');
+insert into jugadores (dni,nombre,apellidos,edad,id_equipo) values 
+('1234567a','dani','carvajal',31,1),
+('2345678b','pedri','gonzalez',20,2),
+('3456789c','alvaro','morata',30,3);
+insert into entrenadores (dni,nombre,apellidos,edad,titulo,id_equipo) values 
+('4567891d','carlo','ancelotti',64,'champions',1),
+('5678901e','xavi','hernandez',40,'liga',2),
+('6789123f','pep','guardiola',50,'champions',3);
+insert into temporadas (id,fecha) values 
+(1,'2023-04-21');
+insert into partidos (id,estadio,hora,id_temp) values 
+(1,'santiago bernabeu','2023-04-21',1);
+insert into partidos_tiene_equipos (id_partido,id_equipo_1,id_equipo_2) values 
+(1,1,2);
